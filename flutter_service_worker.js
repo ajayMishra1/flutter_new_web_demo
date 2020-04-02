5ac6c1ed746d99ee4f48ec91f22c478daee537d0
ajay mishra <mishraajay95124@gmail.com>
2020-04-02 20:50:47 +0530

    Built web
    Branch: master
    Commit: 34b468e45b26ecbdc022c495967f0d13b940fde1
    
    package:peanut 3.5.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "baa8f3abbeb0291a8aa71d03f4c7e640",
-"main.dart.js": "0f372a5bfc0ec1c88027f7d935df93f7",
+"main.dart.js": "2870468ac14c139e77549c2e055e5623",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
